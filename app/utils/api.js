e12372aac51bf89d556c29adbefa2dcc1cd00d17
しゅう <44467870+shumm7@users.noreply.github.com>
2024-03-06 03:31:37 +0900

    Update api.js

--- app/utils/api.js
+++ app/utils/api.js
@@ -4,13 +4,7 @@ function fetch(url, options){
             action: "fetch",
             data: {url: url, options: options}
         }
-    ).then( ()=>{
-        
-    }
-    ).catch((error) => {
-        console.log(error);
-        return false;
-    });
+    )
 }
 
 export function getNovelInfo(ncode){
