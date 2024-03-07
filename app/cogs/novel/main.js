16be303e6d3676f4b470dfdd07ce148418b91cfc
しゅう <44467870+shumm7@users.noreply.github.com>
2024-03-07 18:13:21 +0900

    小説ページ：リーダオプションのタブを実装

--- app/cogs/novel/main.js
+++ app/cogs/novel/main.js
@@ -48,7 +48,7 @@ function optionModal(){
     $("#novel_header").after("<aside id='novel-option'></aside>")
     $("#novel-option").before("<div id='novel-option-background'></div>")
     $("#novel-option").append("<div id='novel-option--header'></div>")
-    $("#novel-option").append("<div id='novel-option--ontent'></div>")
+    $("#novel-option").append("<div id='novel-option--contents'></div>")
     
     $("#novel-option-background").on("click", ()=>{
         if($("#novel-option").hasClass("show")){
@@ -65,7 +65,20 @@ function optionModal(){
 
     /* Modal Header */
     $("#novel-option--header").append("<ul></ul>")
-    $("#novel-option--header ul").append("<li class='active'><button type='button' class='novel-option--header-tab novel-option-tab-01'><span class='novel-option--header-tab'>"+"タブ1"+"</span></button></li>")
-    $("#novel-option--header ul").append("<li><button type='button' class='novel-option--header-tab novel-option-tab-02'><span class='novel-option--header-tab'>"+"タブ2"+"</span></button></li>")
+    function addTab(index, title){
+        $("#novel-option--header ul").append("<li class='novel-option--header-tab novel-option-tab-"+index+"'><button type='button'><span class='novel-option--header-tab'>"+title+"</span></button></li>")
+        $("#novel-option--contents").append("<div class='novel-option--content novel-option-tab-"+index+"'></div>")
+        $(".novel-option--header-tab.novel-option-tab-"+index+" button").on("click", ()=>{
+            $("#novel-option .novel-option--header-tab").removeClass("active")
+            $("#novel-option .novel-option--content").removeClass("active")
+            $("#novel-option .novel-option-tab-" + index).addClass("active")
+        })
+    }
+
+    addTab(1, "表示設定")
+    addTab(2, "その他")
+    $("#novel-option .novel-option-tab-1").addClass("active")
+    $(".novel-option--content.novel-option-tab-1").text("1")
+    $(".novel-option--content.novel-option-tab-2").text("2")
 
 }
