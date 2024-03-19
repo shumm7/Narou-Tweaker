293721cee4a99cf80cf30fa771a2d5bdd0b8bfee
しゅう <44467870+shumm7@users.noreply.github.com>
2024-03-20 05:04:56 +0900

    Update main.js

--- app/cogs/novel/main.js
+++ app/cogs/novel/main.js
@@ -222,7 +222,6 @@ function _header(left, right){
     $("#novel_header ul").append('<li class="report"><a href="https://syosetu.com/ihantsuhou/input/ncode/'+index+'/"><i class="fa-solid fa-bullhorn"></i><span class="title">情報提供</span></a></li>')
 
     /* 編集 */
-    
     if(!is_logined_and_self){
         $("#novel_header ul").append('<li class="edit"><a href="https://syosetu.com/usernovelmanage/top/ncode/'+index+'/"><i class="fa-solid fa-pen-to-square"></i><span class="title">編集</span></a></li>')
     }
