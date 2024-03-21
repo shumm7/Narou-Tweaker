c35e0edcee3cfef0ea89bb059c59311580838222
しゅう <44467870+shumm7@users.noreply.github.com>
2024-03-21 21:56:57 +0900

    Update novel.js

--- app/cogs/background/novel.js
+++ app/cogs/background/novel.js
@@ -1,10 +1,8 @@
-import { defaultValue, getCSSRule } from "../../utils/misc.js";
-import { defaultOption, localSkins, localFont } from "../../utils/option.js";
+import { defaultValue, getCSSRule } from "../../utils/misc.js"
+import { defaultOption, localSkins, localFont } from "../../utils/option.js"
 
 export function applySkin(tab){
     chrome.storage.local.get(null, (data) => {
-        const getRule = getCSSRule
-
         const skin_idx = defaultValue(data.selectedSkin, defaultOption.selectedSkin)
         const skins = localSkins.concat(defaultValue(data.skins, defaultOption.skins))
         const skin = skins[skin_idx]
@@ -25,159 +23,190 @@ export function applySkin(tab){
         var rule = ""
 
         /* Skin */
-        rule += getRule("body", [
-            {"background-color": defaultValue(s.novel.background, "#fff")},
-            {"background-image": "none"}
-        ])
-        rule += getRule("#novel_color", [
-            {"color": defaultValue(s.novel.color, "#444")}
-        ])
-        rule += getRule("#novel_contents div.novelrankingtag, #novel_contents .customlayout-color", [
-            {"color": defaultValue(s.novel.color, "#444")}
-        ])
-        rule += getRule("#novel_contents a:link, #novel_contents .customlayout-color a:link", [
-            {"color": defaultValue(s.link.color_link, "#03c")}
-        ])
-        rule += getRule("#novel_contents a:visited, #novel_contents .customlayout-color a:visited", [
-            {"color": defaultValue(s.link.color_visited, "#857")}
-        ])
-        rule += getRule("#novel_contents a:hover, #novel_contents .customlayout-color a:hover", [
-            {"color": defaultValue(s.link.color_hover, "#393")}
-        ])
-        rule += getRule(".novel_sublist2.underline", [
-            {"border-bottom": "1px solid " + defaultValue(s.sublist.color, "#444")}
-        ])
-        rule += getRule("dl.novel_sublist2", [
-            {"border-color": "transparent"}
-        ])
-        rule += getRule("dl.novel_sublist2:hover", [
-            {"border-color": defaultValue(s.sublist.hover, "#9df")}
-        ])
+        console.log(s.novel.background)
+        rule += `
+        body {
+            background-color: ${s.novel.background} !important;
+            background-image: none !important;
+        }
+        #novel_color {
+            color: ${s.novel.color} !important;
+        }
+        #novel_contents div.novelrankingtag,
+        #novel_contents .customlayout-color {
+            color: ${s.novel.color} !important;
+        }
+
+        #novel_contents a:link,
+        #novel_contents .customlayout-color a:link {
+            color: ${s.link.color_link} !important;
+        }
+        #novel_contents a:visited,
+        #novel_contents .customlayout-color a:visited {
+            color: ${s.link.color_visited} !important;
+        }
+
+        #novel_contents a:hover,
+        #novel_contents .customlayout-color a:hover {
+            color: ${s.link.color_hover} !important;
+        }
+        .novel_sublist2.underline {
+            border-bottom: 1px solid ${s.sublist.color} !important;
+        }
+        dl.novel_sublist2 {
+            border-color: transparent !important;
+        }
+        dl.novel_sublist2:hover {
+            border-color: ${s.sublist.hover} !important;
+        }
+        `
 
         /* Expanded Skin */
         if(expand_skin){
             /*if(tab.url.match(/https:\/\/ncode\.syosetu\.com\/n\d{4}[a-z]{2}\/\d+\//)){ /* 本文ページ */
             /* Title */
-            rule += getRule(".contents1", [
-                {"color": defaultValue(s.novel.color, "#444")},
-                {"background-color": defaultValue(s.novel.background, "#fff")},
-                {"background-image": "none"}
-            ])
-            rule += getRule(".contents1 a:link", [
-                {"color": defaultValue(s.link.color_link, "#03c")}
-            ])
-            rule += getRule(".contents1 a:visited", [
-                {"color": defaultValue(s.link.color_visited, "#857")}
-            ])
-            rule += getRule(".contents1 a:hover", [
-                {"color": defaultValue(s.link.color_hover, "#393")}
-            ])
+            rule += `
+            .contents1 {
+                color: ${s.novel.color} !important;
+                background-color: ${s.novel.background} !important;
+                background-image: none !important;
+            }
+            .contents1 a:link {
+                color: ${s.link.color_link} !important;
+            }
+            .contents1 a:visited {
+                color: ${s.link.color_visited} !important;
+            }
+            .contents1 a:hover {
+                color: ${s.link.color_hover} !important;
+            }
             
             /* Footer */
-            rule += getRule(".wrap_menu_novelview_after", [
-                {"background-color": defaultValue(s.novel.background, "#fff")},
-                {"background-image": "none"},
-                {"border-top": "1px solid " + defaultValue(s.sublist.color, "#444")},
-                {"border-bottom": "1px solid " + defaultValue(s.sublist.color, "#444")}
-            ])
-            rule += getRule(".list_menu_novelview_after:first-child", [
-                {"border-left": "1px solid " + defaultValue(s.sublist.color, "#444")}
-            ])
-            rule += getRule(".list_menu_novelview_after", [
-                {"border-right": "1px solid " + defaultValue(s.sublist.color, "#444")}
-            ])
-            rule += getRule("..list_menu_novelview_after a:link", [
-                {"color": defaultValue(s.link.color_link, "#03c")}
-            ])
-            rule += getRule(".list_menu_novelview_after a:visited", [
-                {"color": defaultValue(s.link.color_visited, "#857")}
-            ])
-            rule += getRule(".list_menu_novelview_after a:hover", [
-                {"color": defaultValue(s.link.color_hover, "#393")},
-                {"background": "transparent"}
-            ])
-            rule += getRule(".footerbookmark .booklist .login", [
-                {"background": "transparent"},
-                {"border-color": defaultValue(s.sublist.color, "#444")}
-            ])
-            rule += getRule(".footerbookmark .booklist a.login:hover", [
-                {"background": "transparent"},
-                {"border-color": defaultValue(s.sublist.hover, "#444")}
-            ])
+            .wrap_menu_novelview_after {
+                background-color: ${s.novel.background} !important;
+                background-image: none !important;
+                border-top: 1px solid ${s.sublist.color} !important;
+                border-bottom: 1px solid ${s.sublist.color} !important;
+            }
+            .list_menu_novelview_after:first-child {
+                border-left: 1px solid ${s.sublist.color} !important;
+            }
+            .list_menu_novelview_after {
+                border-right: 1px solid ${s.sublist.color} !important;
+            }
+            .list_menu_novelview_after a:link {
+                color: ${s.link.color_link} !important;
+            }
+            .list_menu_novelview_after a:visited {
+                color: ${s.link.color_visited} !important;
+            }
+            .list_menu_novelview_after a:hover {
+                color: ${s.link.color_hover} !important;
+                background: transparent !important;
+            }
+            .footerbookmark .booklist .login {
+                background: transparent !important;
+                border-color: ${s.sublist.color} !important;
+            }
+            .footerbookmark .booklist a.login:hover {
+                background: transparent !important;
+                border-color: ${s.sublist.hover} !important;
+            }
 
             /* Novel Info */
-            rule += getRule("#contents_main", [
-                {"color": defaultValue(s.novel.color, "#444")},
-                {"background-color": defaultValue(s.novel.background, "#fff")},
-                {"background-image": "none"}
-            ])
-            rule += getRule("#contents_main a:link", [
-                {"color": defaultValue(s.link.color_link, "#03c")}
-            ])
-            rule += getRule("#contents_main a:visited", [
-                {"color": defaultValue(s.link.color_visited, "#857")}
-            ])
-            rule += getRule("#contents_main a:hover", [
-                {"color": defaultValue(s.link.color_hover, "#393")}
-            ])
+            #contents_main {
+                color: ${s.novel.color} !important;
+                background-color: ${s.novel.background} !important;
+                background-image: none !important;
+            }
+            #contents_main a:link {
+                color: ${s.link.color_link} !important;
+            }
+            #contents_main a:visited {
+                color: ${s.link.color_visited} !important;
+            }
+            #contents_main a:hover {
+                color: ${s.link.color_hover} !important;
+            }
 
-            rule += getRule("#noveltable1 th, #noveltable2 th, #onazi .th", [
-                {"color": defaultValue(s.novel.color, "#444")},
-                {"background": defaultValue(s.novel.background_second, "#fff")}
-            ])
-            rule += getRule("#noveltable1 th, #noveltable2 th, #noveltable1 td, #noveltable2 td, h1, #onazi", [
-                {"border-color": defaultValue(s.sublist.color, "#444")},
-            ])
+            #noveltable1 th, #noveltable2 th, #onazi .th {
+                color: ${s.novel.color} !important;
+                background: ${s.novel.background_second} !important;
+            }
+            #noveltable1 th, #noveltable2 th,
+            #noveltable1 td, #noveltable2 td,
+            h1,
+            #onazi {
+                border-color: ${s.sublist.color} !important;
+            }
 
             /* Attention */
-            rule += getRule("#novel_attention, .box_announce_bookmark", [
-                {"color": defaultValue(s.novel.color, "#444")},
-                {"background-color": defaultValue(s.novel.background, "#fff")},
-                {"border-color": defaultValue(s.novel.background_second, "#444")}
-            ])
+            #novel_attention,
+            .box_announce_bookmark {
+                color: ${s.novel.color} !important;
+                background-color: ${s.novel.background} !important;
+                border-color: ${s.novel.background_second} !important;
+            }
+            `
         }
 
         /* Novel Page */
-        if(novel_css) {
-            rule += getRule(".narou-tweaker .novel-titles a", [
-                {"color": "inherit"}
-            ])
-            rule += getRule(".narou-tweaker .novel-titles .novel-title, .narou-tweaker .novel-titles .novel-author, .narou-tweaker .novel-chapter", [
-                {"color": "#999999"}
-            ])
-            rule += getRule(".narou-tweaker .novel-titles#ep-0 .novel-title, .narou-tweaker .novel-titles#ep-1 .novel-title", [
-                {"color": "inherit"}
-            ])
+        if(novel_css) { //WIP
+            rule += `
+            .narou-tweaker .novel-titles a {
+                color: inherit !important;
+            }
+            .narou-tweaker .novel-titles .novel-title, .narou-tweaker .novel-titles .novel-author, .narou-tweaker .novel-chapter {
+                color: #999999 !important;
+            }
+            .narou-tweaker .novel-titles#ep-0 .novel-title, .narou-tweaker .novel-titles#ep-1 .novel-title {
+                color: inherit !important;
+            }
+            `
         }
         
         /* Custom CSS */
-        rule += getRule(".narou-tweaker #novel_header ul li > a, .narou-tweaker #novel_header ul li > a:link, .narou-tweaker #novel_header ul li > a:visited, .narou-tweaker #novel_header ul li > form", [
-            {"color": defaultValue(s.sublist.color, "#444")},
-        ])
-        rule += getRule(".narou-tweaker #novel_header ul li a:hover, .narou-tweaker #novel_header ul li a:active, .narou-tweaker #novel_header ul li > form:hover, .narou-tweaker #novel_header ul li > form:active", [
-            {"color": defaultValue(s.sublist.hover, "#444")},
-        ])
-        rule += getRule(".narou-tweaker #novel_header_right ul li > a, .narou-tweaker #novel_header_right ul li > a:link, .narou-tweaker #novel_header_right ul li > a:visited, .narou-tweaker #novel_header_right ul li > form", [
-            {"color": defaultValue(s.sublist.color, "#444")},
-        ])
-        rule += getRule(".narou-tweaker #novel_header_right ul li > a:hover, .narou-tweaker #novel_header_right ul li > a:active, .narou-tweaker #novel_header_right ul li > form:hover, .narou-tweaker #novel_header_right ul li > form:active", [
-            {"color": defaultValue(s.sublist.hover, "#444")},
-        ])
+        rule += `
+        .narou-tweaker-header #novel_header ul li > a,
+        .narou-tweaker-header #novel_header ul li > a:link,
+        .narou-tweaker-header #novel_header ul li > a:visited,
+        .narou-tweaker-header #novel_header ul li > form {
+            color: ${s.sublist.color} !important;
+        }
+        .narou-tweaker-header #novel_header ul li a:hover,
+        .narou-tweaker-header #novel_header ul li a:active,
+        .narou-tweaker-header #novel_header ul li > form:hover,
+        .narou-tweaker-header #novel_header ul li > form:active {
+            color: ${s.sublist.hover} !important;
+        }
+        .narou-tweaker-header #novel_header_right ul li > a,
+        .narou-tweaker-header #novel_header_right ul li > a:link,
+        .narou-tweaker-header #novel_header_right ul li > a:visited,
+        .narou-tweaker-header #novel_header_right ul li > form {
+            color: ${s.sublist.color} !important;
+        }
+        .narou-tweaker-header #novel_header_right ul li > a:hover,
+        .narou-tweaker-header #novel_header_right ul li > a:active,
+        .narou-tweaker-header #novel_header_right ul li > form:hover,
+        .narou-tweaker-header #novel_header_right ul li > form:active {
+            color: ${s.sublist.hover} !important;
+        }
+        `
 
         chrome.scripting.insertCSS({
             css: rule,
             target: {
                 tabId: tab.id,
             }
+        }, function(){
+            data.appliedSkinCSS = rule
+            chrome.storage.local.set({appliedSkinCSS: data.appliedSkinCSS});
         })
-        data.appliedSkinCSS = rule
-        chrome.storage.local.set({appliedSkinCSS: data.appliedSkinCSS});
     });    
 }
 
 export function applyFont(tab){
     chrome.storage.local.get(null, (data) => {
-        const getRule = getCSSRule
         var rule = ""
 
         if (data.appliedFontCSS!=undefined){
@@ -202,34 +231,36 @@ export function applyFont(tab){
         }else{
             fontFamily_Current = localFont["font-family"][fontFamily]
         }
-        rule += getRule("#novel_honbun", [
-            {"line-height": lineHeight + "%"},
-            {"font-size": fontSize + "%"},
-        ])
-        rule += getRule("body#container, #novel_color, #contents_main", [
-            {"font-family": fontFamily},
-            {"text-rendering": textRendering}
-        ])
-        rule += getRule(".novel-option--font-button#gothic", [
-            {"font-family": localFont["font-family"].gothic},
-        ])
-        rule += getRule(".novel-option--font-button#serif", [
-            {"font-family": localFont["font-family"].serif},
-        ])
-        rule += getRule(".novel-option--font-button#custom", [
-            {"font-family": fontFamily_Custom},
-        ])
-        rule += getRule("#novel_honbun, #novel_a, #novel_p", [
-            {"max-width": "100vw"},
-            {"width": width + "px"}
-        ])
-        rule += getRule("#novel_color, .contents1", [
-            {"max-width": "100vw"},
-            {"width": "calc(max("+width+"px, 730px))"}
-        ])
-        
-
-        
+        rule += `
+        #novel_honbun {
+            line-height: ${lineHeight}% !important;
+            font-size: ${fontSize}% !important;
+        }
+        body#container, #novel_color, #contents_main {
+            font-family: ${fontFamily_Current} !important;
+            text-rendering: ${textRendering} !important;
+        }
+        .novel-option--font-button#gothic {
+            font-family: ${localFont["font-family"].gothic} !important;
+        }
+        .novel-option--font-button#serif {
+            font-family: ${localFont["font-family"].serif} !important;
+        }
+        .novel-option--font-button#custom {
+            font-family: ${fontFamily_Custom} !important;
+        }
+        #novel_honbun,
+        #novel_a,
+        #novel_p {
+            max-width: 100vw !important;
+            width: ${width}px !important;
+        }
+        #novel_color,
+        .contents1 {
+            max-width: 100vw !important;
+            width: calc(max(${width}px, 730px)) !important;
+        }
+        `
         chrome.scripting.insertCSS({
             css: rule,
             target: {
@@ -237,6 +268,6 @@ export function applyFont(tab){
             }
         })
         data.appliedFontCSS = rule
-        chrome.storage.local.set({appliedFontCSS: data.appliedFontCSS});
+        chrome.storage.local.set({appliedFontCSS: data.appliedFontCSS})
     })
 }
