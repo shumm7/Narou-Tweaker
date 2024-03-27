8f4713ca39632b94d28cf433f5bbe7921bed6627
しゅう <44467870+shumm7@users.noreply.github.com>
2024-03-28 04:34:21 +0900

    Update background.js

--- app/cogs/background/background.js
+++ app/cogs/background/background.js
@@ -2,7 +2,6 @@ import { updateOption } from "../../utils/option.js";
 import { actionListener } from "./action.js";
 import { messageListener } from "./process.js";
 import { sidepanelListener } from "./sidepanel.js";
-import { skinUpdateListener } from "./skin.js";
 
 /* Update Option Data */
 updateOption()
