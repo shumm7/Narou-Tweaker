69cfeb65bbd6ce9327d7dafebebc9ebd6c292fbb
しゅう <44467870+shumm7@users.noreply.github.com>
2024-03-29 02:12:52 +0900

    小説ページ：スキンCSSのHTMLタグインジェクション対策

--- app/cogs/novel/skin/main.js
+++ app/cogs/novel/skin/main.js
@@ -1,24 +1,20 @@
 
 chrome.storage.local.get(["appliedSkinCSS", "additionalCSS"], (data)=>{
     if("appliedSkinCSS" in data){
-        $("html").append(`<style type="text/css" id="narou-tweaker-style--skin" class="narou-tweaker-style">${data.appliedSkinCSS}</style>`)
+        var l = $(`<style type="text/css" id="narou-tweaker-style--skin" class="narou-tweaker-style"></style>`)
+        l.text(data.appliedSkinCSS)
+        $("html").append()
     }else{
         $("html").append(`<style type="text/css" id="narou-tweaker-style--skin" class="narou-tweaker-style"></style>`)
     }
     if("additionalCSS" in data){
-        $("html").append(`<style type="text/css" id="narou-tweaker-style--user-css" class="narou-tweaker-style">${data.additionalCSS}</style>`)
+        var l = $(`<style type="text/css" id="narou-tweaker-style--user-css" class="narou-tweaker-style"></style>`)
+        l.text(data.additionalCSS)
+        $("html").append()
     }else{
         $("html").append(`<style type="text/css" id="narou-tweaker-style--user-css" class="narou-tweaker-style"></style>`)
     }
 })
-
-
-chrome.storage.local.get(["appliedSkinCSS"], (data)=>{
-    if("appliedSkinCSS" in data){
-        $("html").append(`<style type="text/css" id="narou-tweaker-style--skin" class="narou-tweaker-style">${data.appliedSkinCSS}</style>`)
-    }
-})
-
 
 chrome.storage.local.onChanged.addListener(function(changes){
     if(changes.appliedSkinCSS!=undefined){
