ba1a57fc9acce06b6164befbff4f27e8c430f686
しゅう <44467870+shumm7@users.noreply.github.com>
2024-04-11 19:54:15 +0900

    小説ページ：縦書き

--- app/cogs/novel/main.js
+++ app/cogs/novel/main.js
@@ -3,6 +3,7 @@ import { checkNovelPageDetail } from "./utils.js";
 import { _authorSkin } from "./_skin.js";
 import { _novel } from "./_novel.js";
 import { _optionModal } from "./_modal.js";
+import { _tategaki } from "./_vertical.js";
 
 chrome.storage.local.get(null, (data) => {
     /* Header */
@@ -21,6 +22,7 @@ chrome.storage.local.get(null, (data) => {
 
         if(checkNovelPageDetail()=="novel"){
             _novel()
+            _tategaki()
         }
     }
     
