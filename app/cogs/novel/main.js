e85e9374c7cc514d602d77633d52f58c3ecb2118
しゅう <44467870+shumm7@users.noreply.github.com>
2024-04-11 20:06:45 +0900

    ファイル統合

--- app/cogs/novel/main.js
+++ app/cogs/novel/main.js
@@ -1,9 +1,8 @@
 import { _header, changeHeaderScrollMode } from "./_header.js";
 import { checkNovelPageDetail } from "./utils.js";
 import { _authorSkin } from "./_skin.js";
-import { _novel } from "./_novel.js";
+import { _novel, _tategaki } from "./_novel.js";
 import { _optionModal } from "./_modal.js";
-import { _tategaki } from "./_vertical.js";
 
 chrome.storage.local.get(null, (data) => {
     /* Header */
