175d6a77140d7a67692057409914c7ba04763c37
しゅう <44467870+shumm7@users.noreply.github.com>
2024-05-26 18:21:52 +0900

    小説ページ：一部ページでエラーが発生していた問題を修正
    今度こそ、感想返信時に既読が反映されるようになりました。

--- app/cogs/novel/_novelcom.js
+++ app/cogs/novel/_novelcom.js
@@ -1,30 +1,31 @@
-import { indexToNcode } from "../../utils/text"
+import { indexToNcode } from "/utils/text.js"
 
 export function _novelcom(){
     _readImpression()
 }
 
 function _readImpression(){
-if(location.pathname.match(/^\/impressionres\/confirm\/ncode\/\d*.*\/kanrino\/\d*\/*/))
-    var m = location.pathname.match(/^\/impressionres\/confirm\/ncode\/(\d*).*\/kanrino\/(\d*)\/*/)
-    const ncode = indexToNcode(m[1])
-    const kanrino = m[2]
+    if(location.pathname.match(/^\/impressionres\/confirm\/ncode\/\d+.*\/kanrino\/\d+\/*/)){
+        var m = location.pathname.match(/^\/impressionres\/confirm\/ncode\/(\d+).*\/kanrino\/(\d+)\/*/)
+        const ncode = indexToNcode(m[1])
+        const kanrino = m[2]
 
-    chrome.storage.local.get(null, function(data){
-        if(data.workspaceImpressionMarkAsReadWhenReply && data.workspaceImpressionMarkedButton){
-            if($("form #rescomment").length){
+        chrome.storage.local.get(null, function(data){
+            if(data.workspaceImpressionMarkAsReadWhenReply && data.workspaceImpressionMarkedButton){
+                if($("form #rescomment").length){
 
-                chrome.storage.sync.get(null, function(data_s){
-                    $(document).on('submit','form:has(#rescomment)', function(){
-                        if(data_s.workspaceImpressionMarked==undefined){l.workspaceImpressionMarked = {}}
-                        if(!(ncode in data_s.workspaceImpressionMarked)){
-                            data_s.workspaceImpressionMarked[ncode] = []
-                        }
-                        data_s.workspaceImpressionMarked[ncode].push(kanrino)
-                        chrome.storage.sync.set({workspaceImpressionMarked: data_s.workspaceImpressionMarked})
+                    chrome.storage.sync.get(null, function(data_s){
+                        $(document).on('submit','form:has(#rescomment)', function(){
+                            if(data_s.workspaceImpressionMarked==undefined){l.workspaceImpressionMarked = {}}
+                            if(!(ncode in data_s.workspaceImpressionMarked)){
+                                data_s.workspaceImpressionMarked[ncode] = []
+                            }
+                            data_s.workspaceImpressionMarked[ncode].push(kanrino)
+                            chrome.storage.sync.set({workspaceImpressionMarked: data_s.workspaceImpressionMarked})
+                        })
                     })
-                })
+                }
             }
-        }
-    })
+        })
+    }
 }
