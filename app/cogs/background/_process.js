9324af98cc75f992d9bde1839793fad14862bdeb
しゅう <44467870+shumm7@users.noreply.github.com>
2024-06-16 20:27:53 +0900

    Update _process.js

--- app/cogs/background/_process.js
+++ app/cogs/background/_process.js
@@ -25,6 +25,14 @@ export function messageListener(){
                 return true
             }
             
+        }else if (message.action == "fetchWrap"){
+            try{
+                fetchWrap(message.data.url, message.data.post, message.data.header, message.data.callback, message.data.charset)
+                sendResponse({action: "fetch", result: {}, success: true, id: message.id, message: message.data})
+            }catch(e){
+                sendResponse({action: "fetch", result: e, success: false, id: message.id, message: message.data})
+            }
+            return true
         }else if(message.action == "downloads"){
             chrome.downloads.download({
                 url: message.data.url,
@@ -37,4 +45,75 @@ export function messageListener(){
         sendResponse()
         return
     })
+}
+
+function fetchWrap(_url, _post, _header, _callback, _charset) {
+    var method = "GET";
+    var body = undefined;
+    var headers = {};
+
+    if (_header) {
+        headers = JSON.parse(JSON.stringify(_header));
+    }
+
+    if (_post) {
+        method = "POST";
+        body = Object.keys(_post)
+            .map((key) => key + "=" + encodeURIComponent(_post[key]))
+            .join("&");
+        if (!headers['Content-Type']) {
+            headers['Content-Type'] = 'application/x-www-form-urlencoded; charset=utf-8';
+        }
+    }
+
+    var _timeout = setTimeout(function() {
+        _callback("");
+    }, 5000);
+
+    fetch(_url, {
+            method,
+            headers,
+            body
+        })
+        .then(res => {
+            clearTimeout(_timeout);
+
+            if (res.ok) {
+
+                if (!_charset) {
+                    res = res.text();
+                }
+
+                return res;
+            } else {
+                return false;
+            }
+        })
+        .then(res => {
+            clearTimeout(_timeout);
+
+            if (!res) {
+                _callback("");
+                return;
+            }
+
+            if (_charset) {
+                !async function() {
+                    const ab = await res.arrayBuffer();
+                    const td = new TextDecoder(_charset);
+                    res = td.decode(ab);
+
+                    //console.log("fetch res=", res);
+
+                    _callback(res);
+                }();
+            } else {
+                //console.log("fetch res=", res);
+                _callback(res);
+            }
+        })
+        .catch(error => {
+            _callback("");
+            return;
+        });
 }
